1de368eb426bab0700a30c93b80661e1a78302d6
Kyro <mariogallardo1331@yahoo.com>
2021-11-17 20:10:23 -0600

    touching up fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,14 +17,24 @@ for(var i = 0; i < enemyNames.length; i++) {
 
 // fight funtion
 var fight = function(enemyName) {
-    // Alert players thay they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    while(playerHealth > 0 && enemyHealth > 0) {
+        //ask player if they'd like to fight or run
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        // if player picks "skip" confirm and then stop the loop
+        if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-    //ask player if they'd like to fight or run
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        // if yes (true), leave fight
+        if (confirmSkip) {
+        window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney)
+        break;
+      }
+    }
 
-    // if player choses to fight, fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
@@ -34,6 +44,12 @@ var fight = function(enemyName) {
         //check enemy's health 
         if (enemyHealth <=0) {
             window.alert(enemyName + " has died!");
+
+        // award player money for winning 
+        playerMoney = playerMoney + 20;
+
+        // leave while() loop since enemy is dead
+        break;
         } else {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
@@ -46,31 +62,17 @@ var fight = function(enemyName) {
         // check player's health
         if (playerHealth <=0) {
             window.alert(playerName + " has died!");
-        } else {
-          window.alert(playerName + " still has " + playerHealth + " health left.");
+            // leave while() loop if player is dead 
+            break;
+        }   else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-        // if player choses to skip
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-        // if yess (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playMoney for skipping
-            playerMoney = playerMoney - 2;
-        }
-        //if no (false), ask question again by running fight() agin
-        else {
-            fight();
-        }
-        // if player did not chose 1 or 2 prompt
-    } else {
-        window.alert("You need to choose a vaild option. Try again!");
-    };
-}
+    }
+};
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-} 
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
---
+++ assets/js/practice.js
@@ -0,0 +1,47 @@
+if(parameter) {
+// action
+} else if (parameter) {
+//action
+} else {
+    //action
+}
+
+var score = 3
+
+if (score < 3) {
+    console.log("bummer")
+} else if (score === 3) {
+    console.log("you win!")
+} else {
+    console.log("You loose")
+}
+
+// 3 tyoes of pop up windoes
+
+var alertPrompt = alert("I am sending a message to the user.")
+
+var promptPrompt = prompt("Asking for written user response.")
+
+var confirmPrompt = confirm("Asking the user a true/false bolean question.")
+
+var numArray = [0, 1, 2, 3]
+
+var bolean = [true, true, false]
+
+0 === 0
+
+"0" == 0
+
+parseInt("9") = 9
+JSON.stringify(9) = "9"
+
+var peopleObject = [
+    {
+        "name": "Stephanie",
+        "location": "Atlanta"
+    },
+    {
+        "name": "Mario",
+        "location": "Austin"
+    }
+]
